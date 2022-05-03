a14b416e86774de9145e90b44a70e2b95f834d67
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 13:34:51 +0100

    Update test.php

--- web/test.php
+++ web/test.php
@@ -5,6 +5,8 @@ use NitroPack\PurgeType;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+echo("test.php");
 $nitro = new NitroPack('site_id', 'secret');
 
 $nitro->purgeCache('https://example.com/', null, PurgeType::COMPLETE, 'TEST');
+?>
