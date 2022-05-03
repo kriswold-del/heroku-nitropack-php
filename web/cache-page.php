062e8abf9b758ed2c5794fdcea1364a20044f151
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 16:18:35 +0100

    Update cache-page.php

--- web/cache-page.php
+++ web/cache-page.php
@@ -4,7 +4,7 @@ require_once('autoload.php');
 if(isset($_GET['url']))
 {
 $nitro = new NitroPack\SDK\NitroPack('UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue', '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea');
-$nitro->runWarmup('https://electromarket.co.uk/'.$_GET['url']);
+$nitro->Api->runWarmup('https://electromarket.co.uk/'.$_GET['url']));
 echo ('https://electromarket.co.uk/'.$_GET['url']);
 }
 ?>
