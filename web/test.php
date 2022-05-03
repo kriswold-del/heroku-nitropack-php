2f509ef4fa89b134d2bdef92ce147425498cf61d
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 13:36:00 +0100

    Update test.php

--- web/test.php
+++ web/test.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/vendor/autoload.php';
 
 echo("test.php");
-$nitro = new NitroPack('site_id', 'secret');
 
-$nitro->purgeCache('https://example.com/', null, PurgeType::COMPLETE, 'TEST');
+//$nitro = new NitroPack('site_id', 'secret');
+//$nitro->purgeCache('https://example.com/', null, PurgeType::COMPLETE, 'TEST');
 ?>
