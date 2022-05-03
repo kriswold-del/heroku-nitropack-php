e764186089a1e6da3139cd7a2b0b4cb0e238438d
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:37:35 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@ echo("here");
 require_once('autoload.php');
 if(isset($_GET['url']))
 {
-$nitro = new NitroPack\SDK\NitroPack('MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ', 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G');
+$nitro = new NitroPack\SDK\NitroPack('UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue', '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea');
 $nitro->purgeCache('https://electromarket.co.uk/'.$_GET['url']);
 echo ('https://electromarket.co.uk/'.$_GET['url']);
 }
