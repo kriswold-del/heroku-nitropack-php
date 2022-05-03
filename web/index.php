4f50c5ea9db2295e6b40aeff0846ac52f0569131
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:32:43 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,23 +1,12 @@
 <?php
-$url_components = parse_url($url);
-
-// Use parse_str() function to parse the
-// string passed via URL
-parse_str($url_components['query'], $params);
-$url = $params['url'];]
-if($url != ""){
-}
-echo($params['url']." purged");
+echo("here");
 require_once('autoload.php');
+if($_GET['url']){
+$nitro = new NitroPack\SDK\NitroPack('MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ', 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G');
+$nitro->purgeCache('https://electromarket.co.uk/'.$_GET['url'])
+}else{
+echo ("Not Authorized")
+}
 
-// $electromarket_sandbox_site_id= 'MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ';
-// $electromarket_sandbox_secret= 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G';
-//
-// $electromarket_site_id = 'UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue';
-// $electromarket_secret = '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea';
-
-
-$nitro = new NitroPack\SDK\NitroPack($'MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ', 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G');
-$nitro->purgeCache("/beamz-s500-smoke-machine-with-100ml-fluid")
 
 ?>
