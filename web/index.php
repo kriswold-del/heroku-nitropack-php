70110e078a941b152a5e0f44115cd6ed67fe680c
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:36:38 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,13 @@
 <?php
 echo("here");
 require_once('autoload.php');
-if($_GET['url']){
-echo("here");
-//$nitro = new NitroPack\SDK\NitroPack('MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ', 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G');
-//$nitro->purgeCache('https://electromarket.co.uk/test')
-}else{
-echo ("Not Authorized")
+if(isset($_GET['url']))
+{
+$nitro = new NitroPack\SDK\NitroPack('MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ', 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G');
+$nitro->purgeCache('https://electromarket.co.uk/'.$_GET['url']);
+echo ('https://electromarket.co.uk/'.$_GET['url']);
 }
+
 
 
 ?>
