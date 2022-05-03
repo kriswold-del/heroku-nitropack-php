d427cd3d039b2a50dda96d4aead3216e9433d82a
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:00:58 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,7 @@
 <?php
 echo("here");
+require_once('autoload.php');
+$nitro = new NitroPack\SDK\NitroPack('MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ', 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G');
+$nitro->purgeCache("test")
+
 ?>
