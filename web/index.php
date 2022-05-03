91851c49e48528d37f3858875159b61ce72faaa9
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:20:46 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,22 +5,23 @@ error_reporting(E_ALL);
 
 $url = $_GET["url"];
 
-if($url <> ""){
-    require_once('autoload.php');
-
-    $electromarket_sandbox_site_id= 'MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ';
-    $electromarket_sandbox_secret= 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G';
-
-    $electromarket_site_id = 'UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue';
-    $electromarket_secret = '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea';
-
-    $nitro = new NitroPack\SDK\NitroPack($electromarket_site_id, $electromarket_secret);
-
-    $nitro->purgeCache("https://electromarket.co.uk/".$url)
-
-    echo($params['url']." purged");
-}else{
-    echo("Please provide URL Param");
-}
+echo ($url);
+// if($url <> ""){
+//     require_once('autoload.php');
+//
+//     $electromarket_sandbox_site_id= 'MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ';
+//     $electromarket_sandbox_secret= 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G';
+//
+//     $electromarket_site_id = 'UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue';
+//     $electromarket_secret = '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea';
+//
+//     $nitro = new NitroPack\SDK\NitroPack($electromarket_site_id, $electromarket_secret);
+//
+//     $nitro->purgeCache("https://electromarket.co.uk/".$url)
+//
+//     echo($params['url']." purged");
+// }else{
+//     echo("Please provide URL Param");
+// }
 
 ?>
