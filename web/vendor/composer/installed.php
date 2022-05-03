ad92d0576394bbd9047c010fc60a6c728c41ac7d
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 13:51:10 +0100

    hjhgjhg

---
+++ web/vendor/composer/installed.php
@@ -0,0 +1,45 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '418ad1e9f75391257fdd0fa2736c887bcf125466',
+        'name' => 'nitropack/nitropackcloud-sdk',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nitropack/httpclient' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nitropack/httpclient',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'adb9ad359e9a5636926b9f020d0191a64dfac6b4',
+            'dev_requirement' => false,
+        ),
+        'nitropack/nitropackcloud-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '418ad1e9f75391257fdd0fa2736c887bcf125466',
+            'dev_requirement' => false,
+        ),
+        'nitropack/url' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nitropack/url',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'e7863893422269d32f72cb8f90de4b5c5dafb488',
+            'dev_requirement' => false,
+        ),
+    ),
+);
