542f63e0470188ea3e058287fd43db7cca727b39
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:16:43 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,9 @@
 <?php
-$url_components = parse_url($url);
 
-// Use parse_str() function to parse the
-// string passed via URL
-parse_str($url_components['query'], $params);
-$url = $params['url'];]
+$url = $_GET["url"];
+
 if($url != ""){
     require_once('autoload.php');
-
     $electromarket_sandbox_site_id= 'MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ';
     $electromarket_sandbox_secret= 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G';
 
@@ -22,6 +18,5 @@ if($url != ""){
 }else{
     echo("Please provide URL Param");
 }
-
 
 ?>
