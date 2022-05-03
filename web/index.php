92fa99a48b519774638ef533152147a8f2d31033
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:02:12 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,6 +2,6 @@
 echo("here");
 require_once('autoload.php');
 $nitro = new NitroPack\SDK\NitroPack('MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ', 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G');
-$nitro->purgeCache("test")
+$nitro->purgeCache("/beamz-s500-smoke-machine-with-100ml-fluid")
 
 ?>
