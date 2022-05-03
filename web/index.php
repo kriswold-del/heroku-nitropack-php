e2dfdbc8c9e55174efe44b1f1a07ab6dcff63db5
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:13:14 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,26 @@
 <?php
-echo("here");
-require_once('autoload.php');
-$nitro = new NitroPack\SDK\NitroPack('MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ', 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G');
-$nitro->purgeCache("/beamz-s500-smoke-machine-with-100ml-fluid")
+$url_components = parse_url($url);
+
+// Use parse_str() function to parse the
+// string passed via URL
+parse_str($url_components['query'], $params);
+$url = $params['url'];]
+if($url != ""){
+    require_once('autoload.php');
+
+    $electromarket_sandbox_site_id= 'MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ';
+    $electromarket_sandbox_secret= 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G';
+
+    $electromarket_site_id = 'UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue';
+    $electromarket_secret = '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea';
+    
+    $nitro = new NitroPack\SDK\NitroPack($electromarket_site_id, $electromarket_secret);
+    $nitro->purgeCache("/beamz-s500-smoke-machine-with-100ml-fluid")
+
+    echo($params['url']." purged");
+}else{
+    echo("Please provide URL Param");
+}
+
 
 ?>
