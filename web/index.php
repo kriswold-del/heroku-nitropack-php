88d04bbde24dbe1eafbcf0adde05a404b1001a8a
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-03 14:27:37 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -10,12 +10,12 @@ if($_GET["url"]){
     $electromarket_sandbox_site_id = 'MMCxCaRKaBRecqSCPxgOCLwQvVtWLLKQ';
     $electromarket_sandbox_secret = 'm65rqd7GLtgifSigYvO1oGDwEJyeNhrz1kR3LoGZuRunj90wv1wIiK6zf8rIvq3G';
 
-    //$electromarket_site_id = 'UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue';
-    //$electromarket_secret = '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea';
+    $electromarket_site_id = 'UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue';
+    $electromarket_secret = '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea';
 
-    //$nitro = new NitroPack\SDK\NitroPack($electromarket_site_id, $electromarket_secret);
+    $nitro = new NitroPack\SDK\NitroPack($electromarket_site_id, $electromarket_secret);
 
-    //$nitro->purgeCache($url)
+    $nitro->purgeCache($url)
 
      echo($url." purged");
  }else{
