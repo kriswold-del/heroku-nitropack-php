b5d9eb9d3aa688782d113d375ca3fd51a9e94673
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-05-05 12:37:27 +0100

    Update purge.php

--- web/purge.php
+++ web/purge.php
@@ -1,10 +1,11 @@
 <?php
-echo("here");
 require_once('autoload.php');
-if(isset($_GET['url']))
+if(isset($_POST['site_id']) && isset($_POST['site_secret']) && isset($_POST['last_updated']) && isset($_POST['url']))
 {
-$nitro = new NitroPack\SDK\NitroPack('UAdFgvuPmlrcfGwoSLgtSPmPnJoIsmue', '5DoEZSovPPzVivmpyE3NAPVYnscRf5Abs7VuYM8HoTz8PgEedpgOtoPukOc8svea');
-$nitro->purgeCache('https://electromarket.co.uk/'.$_GET['url']);
-echo ('https://electromarket.co.uk/'.$_GET['url']);
+$nitro = new NitroPack\SDK\NitroPack($_POST['site_id'], $_POST['site_secret']);
+//$nitro->purgeCache('https://electromarket.co.uk/'.$_GET['url']);
+echo($nitro->getUrl());
+
+//echo ('https://electromarket.co.uk/'.$_GET['url']);
 }
 ?>
